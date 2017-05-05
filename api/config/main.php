0c91896164292c0448ddcff9bb83347bdb399ddf
wangxin-king <wangxin.king.314@gmail.com>
2017-05-05 17:11:39 +0800

    添加API v1版本模块

--- api/config/main.php
+++ api/config/main.php
@@ -7,29 +7,33 @@ $params = array_merge(
 );
 
 return [
-    'id' => 'app-api',
-    'basePath' => dirname(__DIR__),
+    'id'                  => 'app-api',
+    'basePath'            => dirname(__DIR__),
     'controllerNamespace' => 'api\controllers',
-    'bootstrap' => ['log'],
-    'modules' => [],
-    'components' => [
-        'request' => [
+    'bootstrap'           => ['log'],
+    'modules'             => [
+        'v1' => [
+            'class' => 'api\modules\v1\Module',
+        ],
+    ],
+    'components'          => [
+        'request'      => [
             'csrfParam' => '_csrf-api',
         ],
-        'user' => [
-            'identityClass' => 'common\models\User',
+        'user'         => [
+            'identityClass'   => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-api', 'httpOnly' => true],
+            'identityCookie'  => ['name' => '_identity-api', 'httpOnly' => true],
         ],
-        'session' => [
+        'session'      => [
             // this is the name of the session cookie used for login on the api
             'name' => 'advanced-api',
         ],
-        'log' => [
+        'log'          => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
+            'targets'    => [
                 [
-                    'class' => 'yii\log\FileTarget',
+                    'class'  => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
                 ],
             ],
@@ -37,14 +41,13 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
+        'urlManager'   => [
+            'enablePrettyUrl'     => true,
+            'showScriptName'      => false,
+            'enableStrictParsing' => true,
+            'rules'               => [
             ],
         ],
-        */
     ],
-    'params' => $params,
+    'params'              => $params,
 ];
---
+++ api/modules/v1/controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\rest\Controller;
+
+/**
+ * User controller for the `v1` module
+ */
+class UserController extends Controller
+{
+
+}
